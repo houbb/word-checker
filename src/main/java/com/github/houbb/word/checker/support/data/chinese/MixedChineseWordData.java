14bcc18e037b99990b6af32cb95963cbb4c69a13
ladzhou2019 <58284272+ladzhou2019@users.noreply.github.com>
2020-12-02 10:27:18 +0800

    Update MixedChineseWordData.java
    修复自定义词典不生效

--- src/main/java/com/github/houbb/word/checker/support/data/chinese/MixedChineseWordData.java
+++ src/main/java/com/github/houbb/word/checker/support/data/chinese/MixedChineseWordData.java
@@ -43,7 +43,7 @@ class MixedChineseWordData extends AbstractWordData {
     static {
         try {
             List<String> lines = StreamUtil.readAllLines(WordCheckerConst.SYSTEM_ZH_DICT_PATH);
-            List<String> defineLines = StreamUtil.readAllLines(WordCheckerConst.DEFINE_EN_DICT_PATH);
+            List<String> defineLines = StreamUtil.readAllLines(WordCheckerConst.DEFINE_ZH_DICT_PATH);
             lines.addAll(defineLines);
 
             WORD_MAP = Guavas.newHashMap(lines.size());
